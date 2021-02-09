fd9de5146b7065477d09e94621244dba4dca06df
Dheeraj <dheer047@gmail.com>
2021-02-09 21:47:06 +0530

    changed db env with respect to heroku

--- src/models/index.js
+++ src/models/index.js
@@ -12,11 +12,14 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  // sequelize = new Sequelize(
+  //   config.database,
+  //   config.username,
+  //   config.password,
+  //   config
+  // );
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    config.DATABASE_URL,
   );
 }
 
